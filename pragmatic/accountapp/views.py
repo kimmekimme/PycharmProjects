467cb580c7deebf08423d44b06d18e2122fecfcb
kimmekimme <tjd04@daum.net>
2021-07-01 22:09:26 +0900

    5

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html') #루트/파일명
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld() #객체생성
+        new_hello_world.text = temp
+        new_hello_world.save() #객체 저장
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world}) #루트/파일명
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})  # 루트/파일명
