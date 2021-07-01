674412504115c59db3a758e403af72be81eddc4d
kimmekimme <tjd04@daum.net>
2021-07-02 01:32:11 +0900

    7

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
@@ -9,5 +9,6 @@ app_name = "accountapp"
 
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'), #함수형 베이스는 함수명 바로 넣어주는 형식
+    path('create/', AccountCreateView.as_view(), name='create'), #클래스 베이스 뷰 형식
 ]
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,15 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
 
 def hello_world(request):
     if request.method == "POST":
+
         temp = request.POST.get('hello_world_input')
 
         new_hello_world = HelloWorld() #객체생성
@@ -20,3 +24,11 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})  # 루트/파일명
+
+class AccountCreateView(CreateView):
+    model = User #User: 장고제공 클래스
+    form_class = UserCreationForm #장고제공 클래스
+    success_url = reverse_lazy('accountapp:hello_world') #장고제공 함수(클래스형)
+    template_name = 'accountapp/create.html'
+
+
