346d93a9a39f33dfac297807fe19be4c71101e81
kimmekimme <tjd04@daum.net>
2021-07-02 21:54:03 +0900

    decorator

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,40 +1,46 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
+from accountapp.decorators import account_ownership_required
 
+has_ownership = [
+    account_ownership_required,
+    login_required
+]
 
+@login_required  # 데코레이터
 def hello_world(request):
+    if request.method == "POST":
 
-    if request.user.is_authenticated:
-        if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
 
-            temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()  # 객체생성
+        new_hello_world.text = temp
+        new_hello_world.save()  # 객체 저장
 
-            new_hello_world = HelloWorld() #객체생성
-            new_hello_world.text = temp
-            new_hello_world.save() #객체 저장
-
-            return HttpResponseRedirect(reverse('accountapp:hello_world')) #루트/파일명
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})  # 루트/파일명
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))  # 루트/파일명
     else:
-        return HttpResponseRedirect(reverse('accountapp:login')) #로그인 실패하면 login.html로 전환
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'hello_world_list': hello_world_list})  # 루트/파일명
+
 
 class AccountCreateView(CreateView):
-    model = User #User: 장고제공 클래스
-    form_class = UserCreationForm #장고제공 클래스
-    success_url = reverse_lazy('accountapp:hello_world') #장고제공 함수(클래스형)
+    model = User  # User: 장고제공 클래스
+    form_class = UserCreationForm  # 장고제공 클래스
+    success_url = reverse_lazy('accountapp:hello_world')  # 장고제공 함수(클래스형)
     template_name = 'accountapp/create.html'
+
 
 class AccountDetailView(DetailView):
     model = User
@@ -42,40 +48,22 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')#일반 fuction에 사용할수있는 데코레이터를 class메서드에 사용할수 있게 변환해주는 어노테이션
 class AccountUpdateView(UpdateView):
-    model = User #User: 장고제공 클래스
-    form_class = AccountUpdateForm #장고제공 클래스
-    success_url = reverse_lazy('accountapp:hello_world') #장고제공 함수(클래스형)
+    model = User  # User: 장고제공 클래스
+    form_class = AccountUpdateForm  # 장고제공 클래스
+    success_url = reverse_lazy('accountapp:hello_world')  # 장고제공 함수(클래스형)
     template_name = 'accountapp/update.html'
     context_object_name = 'target_user'
 
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
     context_object_name = 'target_user'
-    #인증구현
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated  and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
+    # 인증구현
